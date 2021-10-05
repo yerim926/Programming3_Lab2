3060012f6817d88c790f7cd8a4ffddd96f2f0e83
yerim926 <yerim926@users.noreply.github.com>
2021-10-05 09:18:04 -0400

    initial commmit

---
+++ Problem1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    public class MyTestClass
+    {
+        public string getUpperCase(string a)
+        {
+            return a.ToUpper();
+        }
+
+        public int getLength(string a)
+        {
+            return a.Length;
+        }
+    }
+
+
+    public class BuiltInDelegateTest
+    {
+        public static void Main(string[] args)
+        {
+
+            MyTestClass c = new MyTestClass();
+
+
+            Func<string, string> aDel = c.getUpperCase;
+
+            Func<string, int> bDel = c.getLength;
+
+            //////////////////////////////
+            Console.Write("Enter word: ");
+            string s = Console.ReadLine();
+            Console.WriteLine("Options:");
+            Console.WriteLine("1: Get UpperCase");
+            Console.WriteLine("2: Get Length");
+            Console.Write("Enter option: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            switch (i)
+            {
+                case 1:
+                    {
+                        aDel = c.getUpperCase;
+
+                        string theWord = s;
+
+
+                        string sUpper;
+
+                        sUpper = aDel.Invoke(theWord);
+                        Console.WriteLine("Uppercase: " + sUpper);
+                    }
+
+                    break;
+                case 2:
+                    {
+                        bDel = c.getLength;
+                        string theWord = s;
+
+                        int sLength;
+
+                        sLength = bDel.Invoke(theWord);
+                        Console.WriteLine("Length: " + sLength);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit...");
+
+
+            Console.ReadKey(); //Halts execution
+
+        }
+
+    }
+}
---
+++ Problem2/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Problem2
+{
+    public class MyTestClass
+    {
+        public string getUpperCase(string a)
+        {
+            return a.ToUpper();
+        }
+
+        public int getLength(string a)
+        {
+            return a.Length;
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MyTestClass c = new MyTestClass();
+        
+        public delegate string MyFunc1(string arg);
+
+        public delegate int MyFunc2(string arg);
+
+
+        //handler method for click on left button
+        private void leftButton_onClick(object sender, RoutedEventArgs e)
+        {
+            MyFunc1 aDel;
+
+            aDel = c.getUpperCase;
+
+            //The argument to be passed to the wrapped method is
+            //saved in variable theWord.
+            string theWord = textbox1.Text;
+
+
+            string sUpper;
+            
+            sUpper = aDel.Invoke(theWord);
+
+            textblock1.Text = sUpper;
+        }
+
+        //handler method for click on right button
+        private void rightButton_onClick(object sender, RoutedEventArgs e)
+        {
+            MyFunc2 bDel;
+
+            bDel = c.getLength;
+
+            string theWord = textbox1.Text;
+
+            int sLength;
+            
+            sLength = bDel(theWord);
+
+
+            textblock1.Text = Convert.ToString(sLength);
+
+        }
+    }
+
+}
